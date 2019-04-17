d04477fc72c243a7bb28b72dc84cb5ec627ce4c3
erik-sandoval <erik.sandoval@icloud.com>
2019-04-17 10:18:36 -0700

    finished content, some ui tweaks

--- animations.js
+++ animations.js
@@ -1,11 +1,14 @@
 const signUpButton = document.querySelector('.hidebutton')
 const pizza = document.querySelector('.pizza-banner')
 const headerText = document.querySelector('h1')
+
 tl = new TimelineMax({})
 
 tl.fromTo(pizza, 2, { x: -600, opacity: 0 }, { x: -101, opacity: 1 })
 tl.staggerTo('.hidetext', .75, { y: -90, ease: Power4.easeOut }, .5);
 tl.from('.hidebutton', 01, { y: 90, ease: Power4.easeOut });
+
+
 
 signUpButton.addEventListener('mouseover', () => {
     TweenMax.to(pizza, 3, { x: "45%", rotation: 360, scale: 2 })
